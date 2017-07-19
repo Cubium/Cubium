f8a3eac1d33227d961e5e6effa3bd30b19a2d36e
JackKiefer <jack.c.kiefer@gmail.com>
2017-07-18 22:53:23 -0600

    reorganize into demo directory

---
+++ demo/demo_addresses.hpp
@@ -0,0 +1,8 @@
+#ifndef DEMO_ADDRESSES_HPP
+#define DEMO_ADDRESSES_HPP
+
+const LogicalAddress la_LSM(1,0);
+const LogicalAddress la_CA(1,1);
+const LogicalAddress la_CB(1,2);
+
+#endif
---
+++ demo/subnetmanager/subnet_manager_driver.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <local_communicator.hpp>
+#include <local_subnet_manager.hpp>
+#include <memory>
+#include <messages/local/local_ack.h>
+#include <messages/local/local_spa_message.h>
+#include <messages/op_codes.h>
+#include <socket/serverSocket.hpp>
+#include "../demo_addresses.hpp"
+
+int main(void)
+{
+  uint16_t port = 3500;
+
+  cubiumServerSocket_t sock = serverSocket_openSocket(port);
+
+  auto routingTable = std::make_shared<RoutingTable>(la_LSM, port);
+
+  std::vector<std::shared_ptr<PhysicalCommunicator>> comms = {
+      std::make_shared<LocalCommunicator>(&sock, routingTable, la_LSM)};
+
+  auto spaCom = std::make_shared<SpaCommunicator>(la_LSM, comms);
+
+  auto manager = std::make_shared<LocalSubnetManager>(spaCom, la_LSM, port);
+  manager->listenMessages();
+
+  return 0;
+}
