f3fa18dbafe5b9a3716d479d84cae68b4cbfdb73
Raul Ramirez <rlramirez12@gmail.com>
2017-07-19 21:44:21 -0600

    Created a Subscription Reply (#9)
    * Created a Subscription Reply
    
    * Reformatted with Clang-Format

---
+++ lib/messages/spa/subscription_reply.h
@@ -0,0 +1,44 @@
+#ifndef SPA_SUBSCRIPTION_REPLY
+#define SPA_SUBSCRIPTION_REPLY
+
+#include <messages/op_codes.h>
+#include <spa_message.h>
+#include <stditn.h>
+
+struct SubscriptionReply
+{
+
+  SubscriptionReply(
+      LogicalAddress destination,
+      LogicalAddress source)
+    : spaMessage(0, 0, 0,
+                 destination,
+                 source,
+                 0, 0x47),
+      dialogId(0),
+      replyType(0) {}
+
+  SubscriptionReply(
+      uint8_t version,
+      uint8_t priority,
+      uint16_t length,
+      LogicalAddress destination,
+      LogicalAddress source,
+      uint16_t dI,
+      uint8_t rT)
+    : spaMessage(version,
+                 priority,
+                 length,
+                 destination,
+                 source,
+                 flags,
+                 0x47),
+      dialogId(dI),
+      replyType(rT) {}
+
+  SpaMessage spaMessage;
+  uint16_t dialogId;
+  uint8_t replyType;
+};
+
+#endif
