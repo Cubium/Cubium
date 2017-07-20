30389cfda90ac7c2b9be12fb5092d9e8a7b83fe5
Sailanarmo <rlramirez12@gmail.com>
2017-07-19 21:43:05 -0600

    Reformatted with Clang-Format

--- lib/messages/spa/subscription_reply.h
+++ lib/messages/spa/subscription_reply.h
@@ -2,50 +2,43 @@
 #define SPA_SUBSCRIPTION_REPLY
 
 #include <messages/op_codes.h>
-#include <stditn.h>
 #include <spa_message.h>
+#include <stditn.h>
 
 struct SubscriptionReply
 {
 
   SubscriptionReply(
-    LogicalAddress destination,
-	LogicalAddress source)
-  : spaMessage(0,0,0,
-			   destination,
-			   source,
-			   0, 0x47),
-	dialogId(0),
-	replyType(0) {}
+      LogicalAddress destination,
+      LogicalAddress source)
+    : spaMessage(0, 0, 0,
+                 destination,
+                 source,
+                 0, 0x47),
+      dialogId(0),
+      replyType(0) {}
 
   SubscriptionReply(
-    uint8_t version,
-	uint8_t priority,
-	uint16_t length,
-	LogicalAddress destination,
-	LogicalAddress source,
-	uint16_t dI,
-	uint8_t rT)
-  : spaMessage(version,
-			   priority,
-			   length,
-			   destination,
-			   source,
-			   flags,
-			   0x47 ),
-	dialogId(dI),
-	replyType(rT) {}
+      uint8_t version,
+      uint8_t priority,
+      uint16_t length,
+      LogicalAddress destination,
+      LogicalAddress source,
+      uint16_t dI,
+      uint8_t rT)
+    : spaMessage(version,
+                 priority,
+                 length,
+                 destination,
+                 source,
+                 flags,
+                 0x47),
+      dialogId(dI),
+      replyType(rT) {}
 
   SpaMessage spaMessage;
   uint16_t dialogId;
   uint8_t replyType;
-
 };
-
-
-
-
-
-
 
 #endif
