c3033b0eb821d3e7fe809e63e6445acd1b02a7ac
Sailanarmo <rlramirez12@gmail.com>
2017-07-20 00:31:49 -0600

    Added a Component A and Component B

---
+++ demo/Component_B/CMakeFiles/component_B.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/component_B.dir/component_B.cpp.o"
+  "component_B.pdb"
+  "component_B"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/component_B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
