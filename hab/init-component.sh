94fdcd6c83bc1ad561f51bfe1047e5f7ceea9404
JackKiefer <jack.c.kiefer@gmail.com>
2017-10-13 16:27:36 -0600

    initial system setup

---
+++ hab/init-component.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+mkdir $1
+
+echo "add_executable($1 $1.cpp)" > $1/CMakeLists.txt
+echo "target_link_libraries($1 Cubium pthread)" >> $1/CMakeLists.txt
+
+name="$1"
+upperName=${name^^}
+capitalName=${name^}
+
+
+echo "#include \"../addresses.hpp\"" > $1/$1.cpp
+echo "#include <component.hpp>" >> $1/$1.cpp
+echo "#include <iostream>" >> $1/$1.cpp
+echo "#include <unistd.h>" >> $1/$1.cpp
+echo "" >> $1/$1.cpp
+echo "#define COMP_NAME $capitalName" >> $1/$1.cpp
+echo "#define COMP_ADDR la_$upperName" >> $1/$1.cpp
+echo "#define MNGR_ADDR la_LSM" >> $1/$1.cpp
+echo "" >> $1/$1.cpp
+echo "class COMP_NAME : public Component" >> $1/$1.cpp
+echo "{" >> $1/$1.cpp
+echo "public:" >> $1/$1.cpp
+echo "  COMP_NAME(std::shared_ptr<SpaCommunicator> com = nullptr) : Component(com, COMP_ADDR, MNGR_ADDR)" >> $1/$1.cpp
+echo "  {" >> $1/$1.cpp
+echo "  }" >> $1/$1.cpp
+echo "" >> $1/$1.cpp
+echo "  void handleSpaData(SpaMessage* message)" >> $1/$1.cpp
+echo "  {" >> $1/$1.cpp
+echo "  }" >> $1/$1.cpp
+echo "" >> $1/$1.cpp
+echo "  void sendData(LogicalAddress destination)" >> $1/$1.cpp
+echo "  {" >> $1/$1.cpp
+echo "    auto payload = \"Test string!\";" >> $1/$1.cpp
+echo "    sendPayload(payload, destination);" >> $1/$1.cpp
+echo "  }" >> $1/$1.cpp
+echo "" >> $1/$1.cpp
+echo "  void init()" >> $1/$1.cpp
+echo "  {" >> $1/$1.cpp
+echo "  }" >> $1/$1.cpp
+echo "};" >> $1/$1.cpp
+echo "" >> $1/$1.cpp
+echo "int main()" >> $1/$1.cpp
+echo "{" >> $1/$1.cpp
+echo "  component_start<COMP_NAME>(COMP_ADDR);" >> $1/$1.cpp
+echo "  return EXIT_SUCCESS;" >> $1/$1.cpp
+echo "}" >> $1/$1.cpp
---
+++ hab/light/light.cpp
@@ -0,0 +1,36 @@
+#include "../addresses.hpp"
+#include <component.hpp>
+#include <iostream>
+#include <unistd.h>
+
+#define COMP_NAME Light
+#define COMP_ADDR la_LIGHT
+#define MNGR_ADDR la_LSM
+
+class COMP_NAME : public Component
+{
+public:
+  COMP_NAME(std::shared_ptr<SpaCommunicator> com = nullptr) : Component(com, COMP_ADDR, MNGR_ADDR)
+  {
+  }
+
+  void handleSpaData(SpaMessage* message)
+  {
+  }
+
+  void sendData(LogicalAddress destination)
+  {
+    auto payload = "Test string!";
+    sendPayload(payload, destination);
+  }
+
+  void init()
+  {
+  }
+};
+
+int main()
+{
+  component_start<COMP_NAME>(COMP_ADDR);
+  return EXIT_SUCCESS;
+}
