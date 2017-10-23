90cac1b3c82fb52d0fb134113c2215fb642e9b90
Jack Kiefer <jack.c.kiefer@gmail.com>
2017-10-23 21:54:18 +0000

    Remove unneeded imports

--- hab/gyro/py_component.py
+++ hab/gyro/py_component.py
@@ -1,7 +1,5 @@
 print 'Initalizing gyro...'
 
-import logging
-import sys
 import time
 
 from Adafruit_BNO055 import BNO055
@@ -17,9 +15,12 @@ def sendData():
     sys, gyro, accel, mag = bno.get_calibration_status()
     return 'H{0:0.2F},R{1:0.2F},P{2:0.2F},S{3},G{4},A{5},M{6}'.format(heading, roll, pitch, sys, gyro, accel, mag)
  
-
 def init():
     if not bno.begin():
         raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
 
-    sys.stdout.write("Initialization success!\n")
+    print "Initialization success!"
+
+init()
+while True:
+    print sendData()
