83e9ade448c0389c25da42f3115490e90fa53437
Jack Conrad Kiefer II <jack.c.kiefer@gmail.com>
2017-10-23 15:59:59 -0600

    Fully implemented gyroscope driver (#33)
    * Proof that the system works using ONLY a local communicator
    
    * More removals
    
    * And just like that, no more spa or physical communicators needed!
    
    * LSM absorbs parent class
    
    * Remove more old commented-out code
    
    * Add some immutability to component members
    
    * Some fixes
    
    * Gyro driver
    
    * Finialize gyro driver
    
    * Remove unneeded imports

---
+++ hab/gyro/py_component.py
@@ -0,0 +1,26 @@
+print 'Initalizing gyro...'
+
+import time
+
+from Adafruit_BNO055 import BNO055
+
+bno = BNO055.BNO055(rst='P9_12', busnum=2)
+
+def handleSpaData():
+    pass
+
+def sendData():
+    time.sleep(1)
+    heading, roll, pitch = bno.read_euler()
+    sys, gyro, accel, mag = bno.get_calibration_status()
+    return 'H{0:0.2F},R{1:0.2F},P{2:0.2F},S{3},G{4},A{5},M{6}'.format(heading, roll, pitch, sys, gyro, accel, mag)
+ 
+def init():
+    if not bno.begin():
+        raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
+
+    print "Initialization success!"
+
+init()
+while True:
+    print sendData()
