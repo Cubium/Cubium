843a6a1419faf6369f5fcc81341d05c504c4a109
USUFox <carson.fox@aggiemail.usu.edu>
2017-11-08 16:34:09 -0700

    Initial python driver for analog UV sensor

--- hab/uv/uv.cpp
+++ hab/uv/uv.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <unistd.h>
 
+#include "python2.7/Python.h"
+#include <stdlib.h>
+
 #define COMP_NAME Uv
 #define COMP_ADDR la_UV
 #define MNGR_ADDR la_LSM
@@ -20,14 +23,56 @@ public:
 
   void sendData(LogicalAddress destination)
   {
-    sleep(1);
-    std::string payload = "UV data!";
-    std::cout << "Sending payload: " << payload << std::endl;
+    Py_Initialize();
+		PyRun_SimpleString("import sys; sys.path.append('.')");
+		PyRun_SimpleString("import py_component");
+
+    PyObject *pName, *pModule, *pDict, *pFunc, *pArgs, *pValue, *pResult;
+
+		pName = PyString_FromString("py_component");
+		pModule = PyImport_Import(pName);
+		if(pModule == NULL) 
+		{
+			std::cout << "Null Module!" << std::endl;
+			PyErr_Print();
+		}
+		pDict = PyModule_GetDict(pModule);
+
+		pFunc = PyDict_GetItemString(pDict, "sendData");
+		//setenv("PYTHONPATH","/usr/lib/python2.7",1);
+
+    pResult = PyObject_CallFunction(pFunc, NULL);
+
+    double payload = PyFloat_AsDouble(pResult);
+
+    std::cout << "Sending SpaData: " << payload << std::endl;
+
     sendPayload(payload, destination);
   }
 
   void init()
   {
+    Py_Initialize();
+		PyRun_SimpleString("import sys; sys.path.append('.')");
+		PyRun_SimpleString("import py_component");
+
+		PyObject *pName, *pModule, *pDict, *pFunc, *pArgs, *pValue, *pResult;
+
+		pName = PyString_FromString("py_component");
+		pModule = PyImport_Import(pName);
+		if(pModule == NULL) 
+		{
+			std::cout << "Null Module!" << std::endl;
+			PyErr_Print();
+		}
+		pDict = PyModule_GetDict(pModule);
+
+		pFunc = PyDict_GetItemString(pDict, "init");
+		//setenv("PYTHONPATH","/usr/lib/python2.7",1);
+
+		//not capturing result, should inits return anything?
+		PyObject_CallFunction(pFunc, NULL);
+		std::cout << "py_component initialized" << std::endl;
   }
 };
 
