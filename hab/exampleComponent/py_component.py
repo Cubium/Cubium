0175be10a4b7aec07e177b34407d567d17eb6123
CommanderScotty <scott.glaittli@aggiemail.usu.edu>
2018-03-26 20:09:41 -0600

    Added example template component

---
+++ hab/exampleComponent/py_component.py
@@ -0,0 +1,23 @@
+# Import component API ex: import Adafruit_MCP9809 as MCP9809
+import Adafruit_MCP9809 as MCP9809
+
+#================
+# Handle SPA data
+#================
+def handleSpaData():
+    pass
+
+#===============================================
+# Returns the data collected from the component.
+#===============================================
+def sendData():
+    # ex: return sensor.readTempC()
+    pass # Delete this line
+
+#=======================
+# Initializes the sensor
+#=======================
+def init():
+    # ex: sensor = MCP9809.MCP9809()
+    #     sensor.begin()
+    pass
