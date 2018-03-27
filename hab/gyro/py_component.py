243d54b0e897668e7464daea9f620445d83097c7
Sailanarmo <rlramirez12@gmail.com>
2018-03-26 23:11:16 -0600

    Got component to initialize, however, the data doesn't send or do anything.

--- hab/gyro/py_component.py
+++ hab/gyro/py_component.py
@@ -4,17 +4,17 @@ import time
 
 from Adafruit_Python_BNO055 import BNO055
 
-#bno = BNO055.BNO055(rst='P9_12', busnum=2)
+#the RST Pin on the Gyroscope.
 bno = BNO055.BNO055(rst=18)
 
 def handleSpaData():
     pass
 
 def sendData():
-    time.sleep(10)
+    time.sleep(1)
     heading, roll, pitch = bno.read_euler()
     sys, gyro, accel, mag = bno.get_calibration_status()
-    print ('H{0:0.2F},R{1:0.2F},P{2:0.2F},S{3},G{4},A{5},M{6}'.format(heading, roll, pitch, sys, gyro, accel, mag))
+    #print ('H{0:0.2F},R{1:0.2F},P{2:0.2F},S{3},G{4},A{5},M{6}'.format(heading, roll, pitch, sys, gyro, accel, mag))
     return 'H{0:0.2F},R{1:0.2F},P{2:0.2F},S{3},G{4},A{5},M{6}'.format(heading, roll, pitch, sys, gyro, accel, mag)
  
 def init():
