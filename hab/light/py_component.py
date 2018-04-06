f9b6304f099e836ff9e0e55cb42355774404ac0b
Zion Steiner <zion.steiner@aggiemail.usu.edu>
2018-04-05 20:40:37 -0600

    Wrote sense light sensor driver. Will need to be modified upon arrival of 10-bit ADC.

---
+++ hab/light/py_component.py
@@ -0,0 +1,26 @@
+import time
+import Adafruit_ADS1x15
+import math
+
+light =Adafruit_ADS1x15.ADS1115()
+
+GAIN = 1
+
+def handleSpaData():
+    pass
+
+def sendData():
+	values = adc.read_adc(2, gain=GAIN)
+
+	values = values * .0048
+	RLDR = (1000.0 * (5-values))/values
+	#Lux = (776897.0 * (math.pow(RLDR, -1.206)))
+	
+	time.sleep(0.5)
+	
+	return values
+
+	
+
+def init():
+    # Handled elsewhere
