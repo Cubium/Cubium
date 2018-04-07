090e700658dc2caf5f289bacd1be0ca277f7c521
GAS Team <getawayspecial@gmail.com>
2018-04-07 17:05:01 +0000

    fixing compiler errors

--- hab/camera/camera.cpp
+++ hab/camera/camera.cpp
@@ -22,34 +22,43 @@ public:
 
   void handleSpaData(SpaMessage* message)
   {
-		auto castMessage = (SpaString*)message;
-		std::string payload(castMessage->st);
+    //Py_Initialize();
+    //PyRun_SimpleString("import sys; sys.path.append('.')");
+    //PyRun_SimpleString("import py_component");
+
+    auto castMessage = (SpaString*)message;
+    std::string payload(castMessage->st);
 
     std::cout << "Payload: " << payload << std::endl;
 
-		pFunc = PyDict_GetItemString(pDict, "handleSpaData");		
-		pyObject_CallFunction(pFunc, payload.c_str());
+    pFunc = PyDict_GetItemString(pDict, "handleSpaData");		
+    char* data = const_cast<char*>(payload.c_str()); // since c_str() returns a const char*
+    PyObject_CallFunction(pFunc, data);
   }
 
   void sendData(LogicalAddress destination)
   {
-		// PYTHON FILE SENDDATA
+    //Py_Initialize();
+    //PyRun_SimpleString("import sys; sys.path.append('.')");
+    //PyRun_SimpleString("import py_component");
+
+    // PYTHON FILE SENDDATA
     pFunc = PyDict_GetItemString(pDict, "sendData");
 	
-		sleep(1);
+    sleep(1);
     pResult = PyObject_CallFunction(pFunc, NULL); // pResult is return of func
-		std::string payload = PyString_AsString(pResult); // pResult to string
+    std::string payload = PyString_AsString(pResult); // pResult to string
     sendPayload(payload, destination);
   }
 
   void init()
   {
-		// INITIALIZATION
+    // INITIALIZATION
     Py_Initialize();
     PyRun_SimpleString("import sys; sys.path.append('.')");
     PyRun_SimpleString("import py_component");
 
-		// IMPORT CORRECT COMPONENT PYTHON FILE
+    // IMPORT CORRECT COMPONENT PYTHON FILE
     pName = PyString_FromString("py_component");
     pModule = PyImport_Import(pName);
     if (pModule == NULL)
@@ -58,18 +67,19 @@ public:
       PyErr_Print();
     }
 
-		// GET MODULES FROM COMPONENT PYTHON FILE
+    // GET MODULES FROM COMPONENT PYTHON FILE
     pDict = PyModule_GetDict(pModule);
 
-		// PYTHON FILE INIT
+    // PYTHON FILE INIT
     pFunc = PyDict_GetItemString(pDict, "init");
+    // setev("PYTHONPATH", "/usr/lib/python2.7", 1);
 
     //not capturing result, should inits return anything?
     PyObject_CallFunction(pFunc, NULL);
     std::cout << "py_component initialized" << std::endl;
 	
-		// SUBSCRIPTIONS
-		subscribe(la_BOOM);
+    // SUBSCRIPTIONS
+    subscribe(la_BOOM);
     sleep(0.5);
     subscribe(la_RADIO);
   }
