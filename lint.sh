2f23c17a1848c307e70e8069ecc2558a52bc9b09
Carson Fox <carson.fox@aggiemail.usu.edu>
2018-05-25 17:27:38 -0600

    Actually enabled checks

--- lint.sh
+++ lint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Run clang-tidy on the sources
 
-if ! [-f compile_commands.json]
+if ! [ -f compile_commands.json ]
 then
     cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .
 fi
 
-find . -iname *.hpp -o -iname *.cpp | xargs clang-tidy -checks=bugprone,clang-analyzer,modernize,performance,portability,readability
+find . -iname *.hpp -o -iname *.cpp | xargs clang-tidy -checks=bugprone-* -header-filter=.* -list-checks
