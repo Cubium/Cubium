7388ccc213835b42dd012a3c654c588286b5abcc
GAS Team <getawayspecial@gmail.com>
2018-05-29 22:33:25 +0000

    Gyro sending data

--- hab/gyro/py_component.py
+++ hab/gyro/py_component.py
@@ -1,27 +1,16 @@
-print 'Initializing gyro...'
+import Adafruit_LSM303
 
-
-#from Adafruit_Python_BNO055 import BNO055
-
-#the RST Pin on the Gyroscope.
-#bno = BNO055.BNO055(rst=18)
+lsm = Adafruit_LSM303.LSM303()
 
 def handleSpaData():
     pass
 
-i = 0.0
-
 def sendData():
-    global i
-    i += 1.0
-    return "gyro data" + str(i)
-#    heading, roll, pitch = bno.read_euler()
-#    sys, gyro, accel, mag = bno.get_calibration_status()
-    #print ('H{0:0.2F},R{1:0.2F},P{2:0.2F},S{3},G{4},A{5},M{6}'.format(heading, roll, pitch, sys, gyro, accel, mag))
-#    return 'H{0:0.2F},R{1:0.2F},P{2:0.2F},S{3},G{4},A{5},M{6}'.format(heading, roll, pitch, sys, gyro, accel, mag)
+    accel, mag = lsm.read()
+    accel_x, accel_y, accel_z = accel
+    mag_x,mag_y,mag_z = mag
+
+    return 'Accel: X={}, Y={}, Z={}; Mag: X={}, Y={}, Z={}'.format(accel_x,accel_y,accel_z,mag_x,mag_y,mag_z)
  
 def init():
-#    if not bno.begin():
-#        raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
-
     print "Initialization success!"
