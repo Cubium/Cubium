8d06cb1d19eba186fb5635a7cd49b27dcb5c9268
JackKiefer <jack.c.kiefer@gmail.com>
2018-06-27 13:39:35 -0600

    Moar docs!

--- lib/messages/op_codes.h
+++ lib/messages/op_codes.h
@@ -3,6 +3,10 @@
 
 #include <stdint.h>
 
+/*
+ * Op-codes for each type of SpaMessage
+ */
+
 const uint8_t op_LOCAL_HELLO              = 0x20; /* 32 */
 const uint8_t op_LOCAL_ACK                = 0x21; /* 33 */
 const uint8_t op_ALL_REGISTERED           = 0x22; /* 34 */
